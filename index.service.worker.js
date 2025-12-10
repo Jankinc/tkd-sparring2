c9f89433aa53269b6c0b44a068a2f779bc91e0d6
Jankinc <86422441+Jankinc@users.noreply.github.com>
2025-12-10 19:03:43 +0800

    update

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1765363828|61233241';
+const CACHE_VERSION = '1765364589|821908337';
 /** @type {string} */
 const CACHE_PREFIX = 'New Game Project-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
